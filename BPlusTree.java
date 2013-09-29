41b39e9e4b7e29da5f4868b13857be7f362c6125
Amy Ho <ah673@cornell.edu>
2013-09-28 22:25:11 -0400

    Overflowed leaves will copy up index to index Node.

--- BPlusTree.java
+++ BPlusTree.java
@@ -3,6 +3,7 @@ package CS4320_HW1;
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.ListIterator;
 import java.util.Map.Entry;
 
 /**
@@ -37,19 +38,64 @@ public class BPlusTree {
 			return;
 		}
 		
-		LeafNode leafNode = findLeafNodeForInsert(root, key);
-		insertIntoLeafNode(leafNode, key, value);
+		// find appropriate parent index node and leaf node for key
+		IndexNode idxNode = findIndexNode(root, key); 
+		if (idxNode == null) {
+			System.out.println("idx node is null");
+		}
+		LeafNode leafNode; 
+		if (idxNode == null) 
+			leafNode = findLeafNodeForInsert(root, key);
+		else 
+			leafNode = findLeafNodeForInsert(idxNode, key); 
+
+		insertIntoTree(idxNode, leafNode, key, value);
 
 	}
 
-	private void insertIntoLeafNode(LeafNode leafNode, int key, String value) {
+	/**
+	 * Helper method to insert() to account for cases of overflow
+	 * @param idxNode
+	 * @param leafNode
+	 * @param key
+	 */
+	private void insertIntoTree(IndexNode idxNode, LeafNode leafNode, int key, String value) {
 		// insert into Leaf
 		leafNode.insertSorted(key, value);
 		
+		// Case: inserting into leaf overflows leaf
 		if (leafNode.isOverflowed()){
-			// insert caused node to overflow - split
-			Entry<Integer,Node> rightLeaf = splitLeafNode(leafNode);
+			System.out.println("you've overflowed me!");
+			Entry<Integer,Node> rightSplit = splitLeafNode(leafNode);
+			int splittingKey = rightSplit.getKey(); 
+			System.out.println("splitting key " + splittingKey);
+			LeafNode rightLeafNode = (LeafNode) rightSplit.getValue(); 
+			manageLeafNodePointers(leafNode, rightLeafNode);
+			// copy up splitting key
+			if (idxNode == null){
+				System.out.println("but idxnode is null");
+				root = new IndexNode(splittingKey, leafNode, rightLeafNode);
+			}
+			else {
+				idxNode.insertSorted(rightSplit, splittingKey); 
+			}
 		}
+	}
+
+	/**
+	 * Rearranges pointers for the leftLeaf's nextLeaf pointer. 
+	 * @param leftLeaf 
+	 * @param rightLeaf
+	 */
+	private void manageLeafNodePointers(LeafNode leftLeaf, LeafNode rightLeaf) {
+		if (leftLeaf.nextLeaf == null )
+			leftLeaf.nextLeaf = rightLeaf;
+		else {
+			rightLeaf.nextLeaf = leftLeaf.nextLeaf;
+			leftLeaf.nextLeaf = rightLeaf; 
+		}
+		
+		
 	}
 
 	/**
@@ -143,15 +189,52 @@ public class BPlusTree {
 		if (theNode == null)
 			return null; 
 		
-		if (theNode.isLeafNode)
+		if (theNode.isLeafNode){
 			// Found the LeafNode
 			return (LeafNode) theNode;
+		}
 		else {
 			// The node is an index node
 			IndexNode indexNode = (IndexNode) theNode;
 			
 			if (key < theNode.keys.get(0)){
 				return findLeafNodeForInsert(indexNode.children.get(0), key); 
+			}
+			else if (key > theNode.keys.get(theNode.keys.size() - 1)) {
+				return findLeafNodeForInsert(indexNode.children.get(indexNode.children.size() - 1), key);
+			}
+			else {
+				System.out.println("in last else");
+				ListIterator<Integer> iterator = indexNode.keys.listIterator();
+				while (iterator.hasNext()){
+					if (iterator.next() > key){
+						System.out.println(iterator.previousIndex());
+					}
+				}
+			}
+		}
+		return null;
+	}
+	
+	private IndexNode findIndexNode(Node node, int key){ 
+		if (!node.isLeafNode){
+			IndexNode idxNode = (IndexNode)node;
+			if (idxNode.children.get(0) instanceof LeafNode)
+				return idxNode;
+			else {
+				if (key < idxNode.keys.get(0))
+					return findIndexNode(idxNode.children.get(0), key);
+				else if (key >= idxNode.keys.get(node.keys.size() - 1))
+					return findIndexNode(idxNode.children.get(idxNode.children.size() - 1 ), key);
+				else {
+					 ListIterator<Node> iterator = idxNode.children.listIterator(); 
+					 while (iterator.hasNext()){
+						 if (iterator.next().keys.get(0) > key){
+							 System.out.println("key is " + key);
+							 return (IndexNode)iterator.previous();
+						 }
+					 }
+				}
 			}
 		}
 		return null;
--- Main.java
+++ Main.java
@@ -8,13 +8,17 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		BPlusTree myTree = new BPlusTree(); 
+		myTree.insert(2, "2");
+		myTree.insert(4, "4");
 		myTree.insert(5, "5");
+		myTree.insert(7, "7");
+		myTree.insert(8, "8");
+		myTree.insert(9, "9");
 		myTree.insert(10, "10");
-		myTree.insert(15, "15");
-		myTree.insert(20, "20");
-		myTree.insert(30, "30");
-		myTree.insert(25, "25");
-//		Utils.printTree(myTree);
+		myTree.insert(11, "11");
+		myTree.insert(12, "12");
+		myTree.insert(13, "13");
+		Utils.printTree(myTree);
 	}
 
 }
