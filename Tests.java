1ba7676992612a372578730818727b0c97fc4134
Amy Ho <ah673@cornell.edu>
2013-10-05 23:58:46 -0400

    Readjusted test cases following decision on how to handle underflows from delete

--- Tests.java
+++ Tests.java
@@ -1,5 +1,3 @@
-//Test
-
 package CS4320_HW1;
 
 import static org.junit.Assert.*;
@@ -9,41 +7,42 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 import org.junit.Test;
 
-
 public class Tests {
 
 	@Test
 	public void testHybrid1() {
 		int primeNumbers[] = new int[] { 2, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14,
 				15, 16 };
-		BPlusTree tree=new BPlusTree();
+		BPlusTree tree = new BPlusTree();
 		Utils.bulkInsert(tree, primeNumbers);
-		
-		String test=outputTree(tree);
-		String correct="@10/@%%@5/8/@@12/14/@%%[(2,2);(4,4);]#[(5,5);(7,7);]#[(8,8);(9,9);]$[(10,10);(11,11);]#[(12,12);(13,13);]#[(14,14);(15,15);(16,16);]$%%";
+
+		String test = outputTree(tree);
+		String correct = "@10/@%%@5/8/@@12/14/@%%[(2,2);(4,4);]#[(5,5);(7,7);]#[(8,8);(9,9);]$[(10,10);(11,11);]#[(12,12);(13,13);]#[(14,14);(15,15);(16,16);]$%%";
 		assertEquals(test, correct);
-		
-	    tree.delete(2);
-	    
-	    test=outputTree(tree);
-	    correct="@8/10/12/14/@%%[(4,4);(5,5);(7,7);]#[(8,8);(9,9);]#[(10,10);(11,11);]#[(12,12);(13,13);]#[(14,14);(15,15);(16,16);]$%%";
-	    assertEquals(test, correct);
-		
+
+		tree.delete(2);
+
+		test = outputTree(tree);
+		correct = "@8/10/12/14/@%%[(4,4);(5,5);(7,7);]#[(8,8);(9,9);]#[(10,10);(11,11);]#[(12,12);(13,13);]#[(14,14);(15,15);(16,16);]$%%";
+		assertEquals(test, correct);
+
 	}
+
 	@Test
 	public void testDeleteLeafNodeMerge() {
-		int testNumbers[] = new int[] { 2, 4, 7, 8, 5, 6};
-		BPlusTree tree=new BPlusTree();
+		int testNumbers[] = new int[] { 2, 4, 7, 8, 5, 6, 3 };
+		BPlusTree tree = new BPlusTree();
 		Utils.bulkInsert(tree, testNumbers);
-		
+
 		tree.delete(6);
-		tree.delete(4);
-		String test=outputTree(tree);
-		
-		String result="[(2,2);(5,5);(7,7);(8,8);]$%%";
+		tree.delete(7);
+		tree.delete(8);
+		String test = outputTree(tree);
+
+		String result = "@4/@%%[(2,2);(3,3);]#[(4,4);(5,5);]$%%";
 		assertEquals(result, test);
 	}
-	
+
 	/**
 	 * return the current tree to console in comparable format
 	 * 
@@ -68,12 +67,12 @@ public class Tests {
 				LeafNode leaf = (LeafNode) target;
 				result += "[";
 				for (int i = 0; i < leaf.keys.size(); i++) {
-					result += "(" + leaf.keys.get(i) + ","
-							+ leaf.values.get(i) + ");";
+					result += "(" + leaf.keys.get(i) + "," + leaf.values.get(i)
+							+ ");";
 				}
-				if(childrenPerIndex.isEmpty()){
-					result += "]$";	
-				}else{
+				if (childrenPerIndex.isEmpty()) {
+					result += "]$";
+				} else {
 					childrenPerIndex.set(0, childrenPerIndex.get(0) - 1);
 					if (childrenPerIndex.get(0) == 0) {
 						result += "]$";
@@ -81,7 +80,7 @@ public class Tests {
 					} else {
 						result += "]#";
 					}
-					
+
 				}
 			} else {
 				IndexNode index = ((IndexNode) target);
@@ -104,7 +103,7 @@ public class Tests {
 			}
 
 		}
-		
+
 		return result;
 
 	}
